de83481da5b46b5a0d5bab2adec15788458cd4b2
obscure723 <obscure723@yone-lab.com>
2019-09-04 14:51:30 +0800

    no message

--- Satoshi.php
+++ Satoshi.php
@@ -8,5 +8,13 @@ class Satoshi
   // サトシの持つポケモンが入る配列
   public $box = [];
 
-  
+  // ポケモンをゲットするメソッド
+  // 持っているポケモンの一覧を確認するメソッド
+  // あるポケモンに攻撃してもらうメソッド
+
+  public function getPokemon($newPokemon)
+  {
+    // サトシが持つboxに新しくポケモンを追加する
+    $this->box[] = $newPokemon;
+  }
 }
--- index.php
+++ index.php
@@ -3,7 +3,10 @@
 // ピカチュウの読み込み
 require_once('Pikachu.php');
 
-$pikachu = new Pikachu(100, 50);
+// サトシの読み込み
+require_once('Satoshi.php');
+
+$pikachu = new Pikachu(100, 50, 'ピカチュウ');
 $pikachu->cry();
 
 echo 'ピカチュウのHPは';
@@ -13,3 +16,9 @@ echo '<br>';
 
 $pikachu->attack();
 // $pikachu->noAttack();
+
+
+// サトシを作成
+$satoshi = new Satoshi();
+
+var_dump($satoshi->box);
